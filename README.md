5a1fd61ee796d262321d195cf57444a4437ba113
JasonEl1 <1eliseijas@hdsb.ca>
2024-01-17 19:22:33 -0500

    add instructions to README

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # transaction-manager
 A simple python command line tool to keep track of your transactions. Intended to be used for cash transactions.
+
+### How to add alias to command line: (MacOS)
+
+Open the terminal and enter this command:
+
+`vim ~/.zshrc`
+
+You can replace `vim` with the alias of any other text editor you wish to use.
+
+Once editing the file, insert the following line:
+
+`alias cash="python3 /Users/yourusername/downloads/transaction-manager/transactions.py`
+
+Note that this is the default download location but you can edit this line to suit your preferences. You can also change the alias name from `cash` to anything else you want it to be.
+
+Save the file and restart the terminal. Entering `cash` into the terminal will start the command line script.
+
+**As of v.0.0.1 the created alias only works if run from within the `transaction-manager` directory. As a workaround, edit the first line of `transactions.py` to include the full absolute path to the balance.txt file.**
--- transactions.py
+++ transactions.py
@@ -1,27 +1,32 @@
-current_balance = float(open("balance.txt", "r").read())
+version = "0.0.1"
+balance_path = "./balance.txt"
+
+try:
+    current_balance = float(open(balance_path, "r").read())
+except:
+    print(f"v.{version}: Please make sure you are running the command from the correct directory")
+    exit()
 
 def print_balance():
-    print(f'Your current balance is ${current_balance}')
+    print(f'Your  balance is ${current_balance}')
 
 def add_amount(amount):
-    with open("balance.txt","w") as file_write:
+    with open(balance_path,"w") as file_write:
         file_write.write(str(current_balance+amount))
         print(f'added {str(amount)} to balance')
         file_write.close()
 
 def remove_amount(amount):
-    file_write = open("balance.txt","w")
+    file_write = open(balance_path,"w")
     file_write.write(str(current_balance-amount))
     print(f'removed {str(amount)} to balance')
     file_write.close()
-
-version = "0.0.1"
 
 print(f'Transaction Manager v.{version}')
 print("enter 'help' for a list of commands")
 print("-----------------------------------")
 
-with open("balance.txt", "r") as file_read:
+with open(balance_path, "r") as file_read:
     current_balance = file_read.read()
     current_balance = float(current_balance)
     file_read.close()
@@ -30,7 +35,7 @@ print_balance()
 while(True):
     command = input()
 
-    with open("balance.txt", "r") as file_read:
+    with open(balance_path, "r") as file_read:
         current_balance = file_read.read()
         current_balance = float(current_balance)
         file_read.close()
@@ -44,7 +49,7 @@ while(True):
         print("exit -> exit Transaction Manager")
         print("-------------")
     elif command == "get":
-        with open("balance.txt", "r") as file_read:
+        with open(balance_path, "r") as file_read:
             current_balance = file_read.read()
             current_balance = float(current_balance)
             file_read.close()
@@ -52,7 +57,7 @@ while(True):
     elif command == "add":
         amount = float(input("Amount to add: "))
         add_amount(amount)
-        with open("balance.txt", "r") as file_read:
+        with open(balance_path, "r") as file_read:
             current_balance = file_read.read()
             current_balance = float(current_balance)
             file_read.close()
@@ -60,7 +65,7 @@ while(True):
     elif command=="remove":
         amount = float(input("Amount to remove: "))
         remove_amount(amount)
-        with open("balance.txt", "r") as file_read:
+        with open(balance_path, "r") as file_read:
             current_balance = file_read.read()
             current_balance = float(current_balance)
             file_read.close()
