1f4a6f17d72417ab96e0eebf644a8bdced0257aa
JasonEl1 <1eliseijas@hdsb.ca>
2024-01-17 23:58:59 -0500

    add express mode

--- README.md
+++ README.md
@@ -16,3 +16,30 @@ Once editing the file, insert the following line:
 Note that this is the default download location but you can edit this line to suit your preferences. You can also change the alias name from `cash` to anything else you want it to be.
 
 Save the file and restart the terminal. Entering `cash` into the terminal will start the command line script.
+
+### How to use transaction-manager:
+
+**There are two ways to use transaction-manager : `normal` mode and `express` mode**
+
+`normal` mode:
+
+enter `cash` (or alternative chosen alias) into the terminal and the tool will start. You will be greeted with a welcome message and will have the option to enter as many commands as you would like before using the `exit` command to close the tool. The `help` command is also available to display a list of valid commands.
+
+***
+
+`express` mode:
+
+enter `cash` (or alternative chosen alias) into the terminal, followed by either one or two arguments. The number of arguments required depends on the specific commands. See the list of commands below for all valid commands available for use in `express` mode. After execution of the specified command, the tool will close itself automatically.
+
+### Commands:
+
+```
+help -> display a list of valid commands
+get -> get current account balance
+set <balance> -> set new account balance
+add <amount> -> add specified amount to account balance
+remove <amount> -> remove specified amount from account balance
+exit -> close transaction-manager
+```
+
+**Note that `exit` command is not available in `express` mode.**
--- transactions.py
+++ transactions.py
@@ -1,4 +1,7 @@
-version = "0.0.2"
+import sys
+
+version = "0.1.0"
+mode = "normal"
 
 user = input("User: ")
 balance_path = f"/Users/{user}/Downloads/transaction-manager/balance.txt"
@@ -8,6 +11,19 @@ try:
 except:
     print(f"v{version}: Please make sure you are running the command from the correct directory")
     exit()
+
+if len(sys.argv)>1:
+    mode = "express"
+
+def print_help():
+    print("COMMANDS")
+    print("-------------")
+    print("get -> get current balance")
+    print("set -> set new balance")
+    print("add -> add funds")
+    print("remove -> remove funds")
+    print("exit -> exit Transaction Manager")
+    print("-------------")
 
 def print_balance():
     print(f'Your  balance is ${current_balance}')
@@ -24,55 +40,111 @@ def remove_amount(amount):
     print(f'removed {str(amount)} to balance')
     file_write.close()
 
-print(f'Transaction Manager v{version}')
-print("enter 'help' for a list of commands")
-print("-----------------------------------")
+if mode == "normal":
 
-with open(balance_path, "r") as file_read:
-    current_balance = file_read.read()
-    current_balance = float(current_balance)
-    file_read.close()
-print_balance()
+    print(f'Transaction Manager v{version}')
+    print("enter 'help' for a list of commands")
+    print("-----------------------------------")
 
-while(True):
-    command = input()
+    with open(balance_path, "r") as file_read:
+        current_balance = file_read.read()
+        current_balance = float(current_balance)
+        file_read.close()
+    print_balance()
+
+    while(True):
+        command = input()
+
+        with open(balance_path, "r") as file_read:
+            current_balance = file_read.read()
+            current_balance = float(current_balance)
+            file_read.close()
+
+        if command=="help":
+            print_help()
+        elif command == "get":
+            with open(balance_path, "r") as file_read:
+                current_balance = file_read.read()
+                current_balance = float(current_balance)
+                file_read.close()
+            print_balance()
+        elif command == "set":
+            amount = float(input("New balance: "))
+            with open(balance_path, "w") as file_write:
+                file_write.write(str(amount))
+                file_write.close()
+            print(f"Balance set to {amount}")
+        elif command == "add":
+            amount = float(input("Amount to add: "))
+            add_amount(amount)
+            with open(balance_path, "r") as file_read:
+                current_balance = file_read.read()
+                current_balance = float(current_balance)
+                file_read.close()
+            print_balance()
+        elif command=="remove":
+            amount = float(input("Amount to remove: "))
+            remove_amount(amount)
+            with open(balance_path, "r") as file_read:
+                current_balance = file_read.read()
+                current_balance = float(current_balance)
+                file_read.close()
+            print_balance()
+        elif command=="exit":
+            exit()
+        else:
+            print("Unknown command. Enter 'help' for a list of commands")
+
+elif mode == "express":
+    arg_count = len(sys.argv)
+    if arg_count>2:
+        print(str(sys.argv[2]))
 
     with open(balance_path, "r") as file_read:
         current_balance = file_read.read()
         current_balance = float(current_balance)
         file_read.close()
 
-    if command=="help":
-        print("COMMANDS")
-        print("-------------")
-        print("get -> get current balance")
-        print("add -> add funds")
-        print("remove -> remove funds")
-        print("exit -> exit Transaction Manager")
-        print("-------------")
-    elif command == "get":
+    if sys.argv[1]=="help":
+        print_help()
+    elif sys.argv[1] == "get":
         with open(balance_path, "r") as file_read:
             current_balance = file_read.read()
             current_balance = float(current_balance)
             file_read.close()
         print_balance()
-    elif command == "add":
-        amount = float(input("Amount to add: "))
-        add_amount(amount)
-        with open(balance_path, "r") as file_read:
-            current_balance = file_read.read()
-            current_balance = float(current_balance)
-            file_read.close()
-        print_balance()
-    elif command=="remove":
-        amount = float(input("Amount to remove: "))
-        remove_amount(amount)
-        with open(balance_path, "r") as file_read:
-            current_balance = file_read.read()
-            current_balance = float(current_balance)
-            file_read.close()
-        print_balance()
-    elif command=="exit":
-        exit()
+    elif sys.argv[1] == "set":
+        if arg_count>2:
+            amount = float(sys.argv[2])
+            with open(balance_path, "w") as file_write:
+                file_write.write(str(amount))
+                file_write.close()
+            print(f"Balance set to {amount}")
+        else:
+            print("No amount set")
+    elif sys.argv[1] == "add":
+        if arg_count>2:
+            amount = float(sys.argv[2])
+            add_amount(amount)
+            with open(balance_path, "r") as file_read:
+                current_balance = file_read.read()
+                current_balance = float(current_balance)
+                file_read.close()
+            print_balance()
+        else:
+            print("No add amount specified")
+    elif sys.argv[1] =="remove":
+        if arg_count>2:
+            amount = float(sys.argv[2])
+            remove_amount(amount)
+            with open(balance_path, "r") as file_read:
+                current_balance = file_read.read()
+                current_balance = float(current_balance)
+                file_read.close()
+            print_balance()
+        else:
+            print("No remove amount specified")
     else:
         print("Unknown command. Enter 'help' for a list of commands")
+
+    exit()
